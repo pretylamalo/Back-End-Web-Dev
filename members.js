8aa7a7cd0354e038e81d7742fd789d6750ca2e1a
PretyLamalo <92161127+pretylamalo@users.noreply.github.com>
2023-02-28 09:09:50 +0800

    Add files via upload
    Exercise Express JS

--- members.js
+++ members.js
@@ -1,24 +1,24 @@
 module.exports  = [
     {   
-        id: 1, 
+        id: s2200285, 
         nim: 101, 
         nama: "Vicky", 
         nomor_hp: "+6281239443635"
     }, 
     {   
-        id: 2, 
+        id: s11920030, 
         nim: 102, 
         nama: "Frances", 
         nomor_hp: "+6282397571006"
     },
     {   
-        id: 3, 
+        id: s2200366, 
         nim: 103, 
         nama: "Cherlita", 
         nomor_hp: "+6281342798343"
     },
     {   
-        id: 4, 
+        id: s21930003, 
         nim: 104, 
         nama: "Joseph", 
         nomor_hp: "+6282260331473"
